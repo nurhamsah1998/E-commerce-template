eae4d46117cbaa560d7caa29183c7fc4a69732b0
nurhamsah dari rumah <nur.hamsah.cash@gmail.com>
2022-09-23 18:41:10 +0700

    installing theming

---
+++ src/Utils/Theme.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { orange, cyan, yellow, red, green } from "@mui/material/colors";
+
+function Theme({ children }) {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: yellow[600],
+      },
+      secondary: {
+        main: cyan[100],
+      },
+      warning: {
+        main: orange[500],
+      },
+      error: {
+        main: red[500],
+      },
+      success: {
+        main: green[500],
+      },
+    },
+    typography: {
+      fontFamily: "Poppins",
+      fontSize: 15,
+      fontWeightMedium: 400,
+      fontWeightLight: 400,
+      fontWeightRegular: 400,
+      fontWeightBold: 900,
+    },
+    components: {
+      MuiButton: {
+        styleOverrides: {
+          root: {
+            textTransform: "capitalize",
+            "&:hover": {
+              boxShadow: "none",
+            },
+            boxShadow: "none",
+          },
+        },
+      },
+    },
+  });
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+}
+
+export default Theme;
---
+++ src/routing.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Routes, Route, Link, Navigate } from "react-router-dom";
+import About from "./About";
+import AboutOne from "./AboutOne";
+import Home from "./Home";
+import Layout from "./Layout";
+import Notfount from "./Notfount";
+
+function Routing() {
+  return (
+    <Routes>
+      <Route
+        path="/belajar-router"
+        element={<Layout />}
+        children={
+          <React.Fragment>
+            <Route path="home" element={<Home />} />
+            <Route path="about-one" element={<AboutOne />} />
+            <Route path="about" element={<About />} />
+          </React.Fragment>
+        }
+      />
+      <Route path="alamat-tidak-ditemukan" element={<Notfount />} />
+      <Route path="*" element={<Navigate to="/alamat-tidak-ditemukan" />} />
+      <Route path="/" element={<Navigate to="/belajar-router/home" />} />
+    </Routes>
+  );
+}
+
+export default Routing;
